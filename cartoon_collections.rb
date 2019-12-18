897b1c0d6ab97c242be501ea2d1e1ec457c138d4
Tanya Bihag <tbihag@gmail.com>
2019-12-18 22:16:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def roll_call_dwarves(dwarves)
   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
