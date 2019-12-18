8c82127d6cda64bcf5b0703f555d3f543fbf85b5
Tanya Bihag <tbihag@gmail.com>
2019-12-18 22:33:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,13 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  
+  short_words.each do |i|
+    if i.split(" ") > 4
+      true 
+    else 
+      false 
+  end 
+  end
 end
 
 def find_the_cheese# code an argument here
