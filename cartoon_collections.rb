647fd74030c72e8465f3366152a0053a1852bfc6
Tanya Bihag <tbihag@gmail.com>
2019-12-18 22:43:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(short_words)
   short_words.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
